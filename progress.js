33c0c6a0a0502595913182b919692396011f0f9d
mdogak <mdogak@gmail.com>
2025-11-19 22:33:30 -0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -818,7 +818,7 @@ function loadFromXml(text) {
             case "BaselineHistory":
                 model.baseline.days = [];
                 model.baseline.planned = [];
-                raw.split("/\r?\n/").forEach(line => {
+                raw.split(/\r?\n/).forEach(line => {
                     line = line.trim();
                     if (!line) return;
                     const parts = line.split(",");
@@ -831,7 +831,7 @@ function loadFromXml(text) {
 
             case "ActualHistory":
                 model.history = [];
-                raw.split("/\r?\n/").forEach(line => {
+                raw.split(/\r?\n/).forEach(line => {
                     line = line.trim();
                     if (!line) return;
                     const parts = line.split(",");
@@ -846,7 +846,7 @@ function loadFromXml(text) {
 
             case "DailyActuals":
                 model.dailyActuals = {};
-                raw.split("/\r?\n/").forEach(line => {
+                raw.split(/\r?\n/).forEach(line => {
                     line = line.trim();
                     if (!line) return;
                     const parts = line.split(",");
