8f282cf4d719a758cda8e059e64de933825af655
mdogak <mdogak@gmail.com>
2025-11-21 23:38:15 -0700

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -1,24 +1,4 @@
-// cookies.js
-const COOKIE_KEY = 'progress_tracker_v3b';
-
-function setCookie(name, value, days = 365) {
-    const d = new Date();
-    d.setTime(d.getTime() + (days*24*60*60*1000));
-    document.cookie = `${name}=${encodeURIComponent(value)};expires=${d.toUTCString()};path=/`;
-}
-
-function getCookie(name) {
-    const n = name + "=";
-    const ca = document.cookie.split(';');
-    for (let c of ca) {
-        c = c.trim();
-        if (c.startsWith(n)) {
-            return decodeURIComponent(c.substring(n.length));
-        }
-    }
-    return null;
-}
-
-function delCookie(name) {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-}
+// Cookie helpers
+function setCookie(name, value, days=365){ const d = new Date(); d.setTime(d.getTime() + (days*24*60*60*1000)); const v = encodeURIComponent(value); document.cookie = `${name}=${v};expires=${d.toUTCString()};path=/`; }
+function getCookie(name){ const n = name + '='; const ca = document.cookie.split(';'); for(let c of ca){ while(c.charAt(0)==' ') c = c.substring(1); if(c.indexOf(n)==0) return decodeURIComponent(c.substring(n.length,c.length)); } return null; }
+function delCookie(name){ document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;` }
