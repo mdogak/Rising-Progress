1281597d240a45c93ed44d6041ef6eb204229e68
mdogak <mdogak@gmail.com>
2026-01-02 19:06:56 -0700

    Add files via upload

--- js/history.js
+++ js/history.js
@@ -331,7 +331,7 @@ export function initHistory({ calcTotalActualProgress, fmtDate, today, computeAn
       end: s.end,
       cost: s.cost,
       perDay: s.perDay,
-      progressValue: s.progressValue,
+      progressValue: (s.totalUnits > 0 ? s.progressValue : ''),
       unitsToDate: s.unitsToDate,
       totalUnits: s.totalUnits,
       unitsLabel: s.unitsLabel,
